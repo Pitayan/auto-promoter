8dc3013a0aa37a14d17a0a2ec74c3d51d04543e8
Yanze Dai <ginoalex8964@yahoo.com>
2024-10-07 06:57:00 +0900

    refactor: adopt node & oxc & biome & rolldown

--- deps.ts
+++
@@ -1,3 +0,0 @@
-// Copyright since 2022 for auto-promoter project.
-
-export * as esbuild from "https://deno.land/x/esbuild@v0.14.48/mod.js"
---
+++ rolldown.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'rolldown'
+
+export default defineConfig([
+  {
+    input: 'src/main.ts',
+    output: {
+      name: 'AutoPromoter',
+      dir: 'dist',
+      format: 'iife',
+      entryFileNames: 'auto-promoter.iife.js',
+    }
+  },
+  {
+    input: 'src/main.ts',
+    output: {
+      name: 'AutoPromoter',
+      dir: 'dist',
+      format: 'esm',
+      entryFileNames: 'auto-promoter.esm.js',
+    }
+  }
+])
